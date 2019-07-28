fcc035a8c5115aba66fd3b10bfd8f82a1796092c
Triet Bui Minh <x3cafe@gmail.com>
2019-07-28 11:47:21 +0700

    Create SuperContainer for Single and Multiple Child

---
+++ super_widgets/lib/super_container.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/widgets.dart';
+
+abstract class SuperBaseContainer extends StatelessWidget {
+  /// Key of the inner widget inside Container.
+  final Key key;
+
+  /// Align the [Stack] within the container.
+  final AlignmentGeometry alignment;
+
+  /// Empty space to surround the [decoration] and [Stack].
+  final EdgeInsetsGeometry margin;
+
+  /// Empty space to inscribe inside the [decoration]. The inner widget is
+  /// placed inside this padding.
+  final EdgeInsetsGeometry padding;
+
+  /// The decoration to paint behind the inner widget.
+  ///
+  /// A shorthand for specifying just a solid color is available in the
+  /// constructor: set the `color` argument instead of the `decoration`
+  /// argument.
+  final Decoration decoration;
+
+  /// Align the [children] of within the Stack.
+  final AlignmentGeometry innerAlignment;
+
+  /// Width of container, by default is double.infinity
+  final double width;
+
+  /// Height of container, by default is double.infinity
+  final double height;
+
+  SuperBaseContainer({
+    Color color,
+    Decoration decoration,
+    this.alignment,
+    this.padding,
+    this.margin,
+    this.key,
+    this.innerAlignment,
+    this.width = double.infinity,
+    this.height = double.infinity,
+  }) : decoration =
+            decoration ?? (color != null ? BoxDecoration(color: color) : null);
+}
+
+abstract class SuperSingleChildContainer extends SuperBaseContainer {
+  /// The inner widget inside
+  final Widget child;
+
+  SuperSingleChildContainer({
+    Color color,
+    Decoration decoration,
+    AlignmentGeometry alignment,
+    EdgeInsetsGeometry padding,
+    EdgeInsetsGeometry margin,
+    Key key,
+    AlignmentGeometry innerAlignment,
+    double width,
+    double height,
+    @required this.child,
+  }) : super(
+          key: key,
+          margin: margin,
+          padding: padding,
+          color: color,
+          decoration: decoration,
+          width: width,
+          height: height,
+          alignment: alignment,
+          innerAlignment: innerAlignment,
+        );
+}
+
+abstract class SuperMultipleChildContainer extends SuperBaseContainer {
+  // The list of inner widgets inside
+  final List<Widget> children;
+
+  SuperMultipleChildContainer({
+    Color color,
+    Decoration decoration,
+    AlignmentGeometry alignment,
+    EdgeInsetsGeometry padding,
+    EdgeInsetsGeometry margin,
+    Key key,
+    AlignmentGeometry innerAlignment,
+    double width,
+    double height,
+    @required this.children,
+  }) : super(
+          key: key,
+          margin: margin,
+          padding: padding,
+          color: color,
+          decoration: decoration,
+          width: width,
+          height: height,
+          alignment: alignment,
+          innerAlignment: innerAlignment,
+        );
+}
