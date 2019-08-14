6fbf5bf84c6a3642d104d49eb1b2b8c7dbddb9f4
Triet Bui Minh <x3cafe@gmail.com>
2019-08-14 15:12:42 +0700

    SafeExpended is a custom Align widget with ability to ignore Expended if `flex` param is null

---
+++ lib/base/safe_expanded.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+
+/// SafeExpanded is a custom widget of [Expanded], which wraps child inside
+/// a Expanded.
+///
+/// If has no padding, just return the child to avoid unnecessary deep level
+class SafeExpanded extends StatelessWidget {
+  /// flex
+  final int flex;
+
+  /// child
+  final Widget child;
+
+  /// Creates a widget that mimics [Container] with combination of common
+  /// painting, positioning, and sizing widgets.
+  SafeExpanded({
+    Key key,
+    this.flex,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget current = child;
+    if (flex != null && flex > 0) {
+      current = Expanded(flex: flex, child: current);
+    }
+
+    return current;
+  }
+}
