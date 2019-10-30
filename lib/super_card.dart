262c399a6ecad9c071eb091cbb7d2dc1461e9e7b
Triet Bui <x3cafe@gmail.com>
2019-10-30 17:34:31 +0700

    [WIP]: Update docs for super_card.dart

--- lib/super_card.dart
+++ lib/super_card.dart
@@ -6,20 +6,47 @@ import 'base/safe_widget.dart';
 
 /// [SuperCard] is a [Container] with [Card] inside
 class SuperCard extends BaseContainer {
-  /// Create new [SuperCard]. Its params is the combination of [Container]'s
-  /// params (has the same param name) and [Card]'s  params (has almost the
-  /// same param name, but if have any duplicated name with its parent,
-  /// then add prefix `child` - for example: if [Container] has `color`
-  /// property, and [Card] also  have `color` property, then the latter will
-  /// be rename to `childColor`).
+  /// Create new [SuperCard] which has a [Container] (parent) with
+  /// a [Card] (child) inside.
+  /// Thus its params is the combination of [Container]'s
+  /// params and [Card]'s  params.
   ///
-  /// The list below only show default params of [Container] :
+  /// Params of the parent widget is:
   ///
-  /// - [alignment] : default is [AlignmentDirectional.topStart]
+  /// - `key` : Key of parent widget
+  /// - `alignment`: an align value from [AlignmentDirectional]
+  /// - `padding`: a padding value from [EdgeInsetsGeometry]
+  /// - `color` and `decoration`: only one params is valid. It's the
+  /// decoration to paint behind the [child].
+  /// - `foregroundDecoration`: It's the decoration to paint in front of the [child].
+  /// - `foregroundDecoration`: It's the decoration to paint in front of the [child].
+  /// - `width` and `height` values include the padding. It can be a double
+  ///  value like 100.0, or [double.infinity], or null, like the size value of
+  ///  [Container]
+  /// - `constraints`: like [BoxConstraints] of [Container]
+  /// - `flex`: same as `flex` value which is used in [Flexible].
+  ///  - If flex=0: [child] will be wrapped inside a [Wrap] widget
+  ///  - If flex>0: [child] will be wrapped inside a [Expanded] with flex value
+  ///  - If flex is null, just return [child] widget
+  /// - `ignoreImplicitWidthHeight`: default is `true`. As we know, `width` and
+  /// `height` of [child] might depends on [alignment] or its parent's size.
+  /// But in some cases we need its size is exactly wrap its [child], for
+  /// example the container of [Text] wrap the size of [Text] instead of
+  /// expanding to full width.
+  /// - `onPressed`: action when press on parent widget
+  /// - `onLongPressed`: action when long-press on parent widget
+  /// - `transform`: the transformation matrix to apply before painting the parent.
   ///
-  /// The list below only show renamed or default params of [Card] :
+  /// Params of the child widget is:
   ///
-  /// - [childKey] : [Key] of the [Card]
+  /// - `childKey`: [Key] of [Card]
+  /// - `childColor`: [Color] of [Card]
+  /// - `elevation`: `elevation` of [Card]. Default is 1.0
+  /// - `shape`: [ShapeBorder] of [Card]. Default is [RoundedRectangleBorder]
+  /// with a circular corner radius of 4.0.
+  /// - `borderOnForeground`: default is true: whether to paint the shape
+  /// border in front of the child of [Card].
+  ///
   SuperCard({
     Key key,
     AlignmentGeometry alignment = AlignmentDirectional.topStart,
