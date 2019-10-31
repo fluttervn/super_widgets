ae67090fe99cc5813176d1e8ec283a2828fc7b8d
Triet Bui <x3cafe@gmail.com>
2019-10-31 14:22:13 +0700

    Update docs for super_container.dart

--- lib/super_container.dart
+++ lib/super_container.dart
@@ -3,9 +3,36 @@ import 'package:flutter/widgets.dart';
 
 import 'base/base_container.dart';
 
-/// [SuperContainer] is a [BaseContainer] with is exposed for public using
+/// [SuperContainer] is a [BaseContainer] with its method exposed for public using
 class SuperContainer extends BaseContainer {
-  /// Create new [SuperContainer].
+  /// Create new [SuperContainer] with the following params:
+  ///
+  /// - `key` : Key of parent widget
+  /// - `alignment`: an align value from [AlignmentDirectional]
+  /// - `color` and `decoration`: only one params is valid. It's the
+  /// decoration to paint behind the [child].
+  /// - `foregroundDecoration`: It's the decoration to paint in front of the [child].
+  /// - `width` and `height` values include the padding. It can be a double
+  ///  value like 100.0, or [double.infinity], or null, like the size value of
+  ///  [Container]
+  /// - `constraints`: like [BoxConstraints] of [Container]
+  /// - `transform`: the transformation matrix to apply before painting the parent.
+  /// - `flex`: same as `flex` value which is used in [Flexible].
+  ///  - If flex=0: [child] will be wrapped inside a [Wrap] widget
+  ///  - If flex>0: [child] will be wrapped inside a [Expanded] with flex value
+  ///  - If flex is null, just return [child] widget
+  /// - `ignoreImplicitWidthHeight`: default is `TRUE`. As we know, `width` and
+  /// `height` of [child] might depends on [alignment] or its parent's size.
+  /// But in some cases we need its size is exactly wrap its [child], for
+  /// example the container of [Text] wrap the size of [Text] instead of
+  /// expanding to full width.
+  /// - `onPressed`: action when press on parent widget
+  /// - `onLongPressed`: action when long-press on parent widget
+  /// - `enableInkWell` and `splashColor`: Wrap this widget inside an
+  /// [InkWell]. Its default value is `FALSE`.
+  ///  - If [enableInkWell] is not true, then just return this [widget].
+  ///  - If [enableInkWell] is true splashColor is null, then `splashColor`
+  /// will be automatically gotten from Theme instead.
   SuperContainer({
     Key key,
     AlignmentGeometry alignment,
@@ -17,11 +44,13 @@ class SuperContainer extends BaseContainer {
     double width,
     double height,
     BoxConstraints constraints,
+    Matrix4 transform,
     int flex,
     bool ignoreImplicitWidthHeight = false,
+    bool enableInkWell = false,
+    Color splashColor,
     VoidCallback onPressed,
     VoidCallback onLongPressed,
-    Matrix4 transform,
     Widget child,
   }) : super(
           key: key,
@@ -34,11 +63,11 @@ class SuperContainer extends BaseContainer {
           width: width,
           height: height,
           constraints: constraints,
+          transform: transform,
           flex: flex,
           ignoreImplicitWidthHeight: ignoreImplicitWidthHeight,
           onPressed: onPressed,
           onLongPressed: onLongPressed,
-          transform: transform,
           child: child,
         );
 }
