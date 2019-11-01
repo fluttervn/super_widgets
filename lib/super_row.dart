99ac0382fa5c563e38ab8c9e44cbdf798611f0fc
Triet Bui <x3cafe@gmail.com>
2019-11-01 16:11:11 +0700

    Update docs for super_row.dart

--- lib/super_row.dart
+++ lib/super_row.dart
@@ -4,25 +4,58 @@ import 'base/base_container.dart';
 
 /// [SuperRow] is a [Container] with [Row] inside
 class SuperRow extends BaseContainer {
-  /// Create new [SuperRow]. Its params is the combination of
-  /// [Container]'s params (has the same param name) and [Row]'s
-  /// params (has almost the same param name, but if have any duplicated name
-  /// with its parent, then add prefix `child` - for example: if [Container]
-  /// has `color` property, and [Row] also  have `color` property,
-  /// then the latter will be rename to `childColor`).
+  /// Create new [SuperRow] which has a [Container]
+  /// (parent) with a [Row] (child) inside.
+  /// Thus its params is the combination of [Container]'s
+  /// params and [Row]'s  params.
   ///
-  /// The list below only show default params of [Container] :
+  /// <b>Params of the parent widget is:</b>
   ///
-  /// - [alignment] : default is [AlignmentDirectional.topStart]
+  /// - `key` : Key of parent widget
+  /// - `alignment`: an align value from [AlignmentDirectional]
+  /// - `color` and `decoration`: only one params is valid. It's the
+  /// decoration to paint behind the [child].
+  /// - `foregroundDecoration`: It's the decoration to paint in front of the [child].
+  /// - `width` and `height` values include the padding. It can be a double
+  ///  value like 100.0, or [double.infinity], or null, like the size value of
+  ///  [Container]
+  /// - `constraints`: like [BoxConstraints] of [Container]
+  /// - `transform`: the transformation matrix to apply before painting the parent.
+  /// - `flex`: same as `flex` value which is used in [Flexible].
+  ///  - If flex=0: [child] will be wrapped inside a [Wrap] widget
+  ///  - If flex>0: [child] will be wrapped inside a [Expanded] with flex value
+  ///  - If flex is null, just return [child] widget
+  /// - `ignoreImplicitWidthHeight`: default is `TRUE`. As we know, `width` and
+  /// `height` of [child] might depends on [alignment] or its parent's size.
+  /// But in some cases we need its size is exactly wrap its [child], for
+  /// example the container of [Text] wrap the size of [Text] instead of
+  /// expanding to full width.
+  /// - `onPressed`: action when press on parent widget
+  /// - `onLongPressed`: action when long-press on parent widget
+  /// - `enableInkWell` and `splashColor`: Wrap this widget inside an
+  /// [InkWell]. Its default value is `FALSE`.
+  ///  - If [enableInkWell] is not true, then just return this [widget].
+  ///  - If [enableInkWell] is true splashColor is null, then `splashColor`
+  /// will be automatically gotten from Theme instead.
   ///
-  /// The list below only show renamed or default params of [Row] :
+  /// <b>Params of the child widget is:</b>
   ///
-  /// - [childKey] : [Key] of the [Row]
-  /// - [children] : default is `<Widget>[]`
-  /// - [mainAxisAlignment] : default is [MainAxisAlignment.start]
-  /// - [mainAxisSize] : default is [MainAxisSize.max]
-  /// - [crossAxisAlignment] : default is [CrossAxisAlignment.center]
-  /// - [verticalDirection] : default is [VerticalDirection.down]
+  /// - `childKey`: [Key] of [Row].
+  /// - `children`: list of children widget of [Row].
+  /// - `mainAxisAlignment`: How the children should be placed along the main
+  /// axis. Defaults to [MainAxisAlignment.start].
+  /// - `mainAxisSize`: How much space should be occupied in the main axis.
+  /// Defaults to [MainAxisSize.max].
+  /// - `crossAxisAlignment`: How the children should be placed along the cross
+  /// axis. Defaults to [CrossAxisAlignment.center].
+  /// - `verticalDirection`: Determines the order to lay children out
+  /// vertically and how to interpret `start` and `end` in the vertical
+  /// direction. Defaults to [VerticalDirection.down].
+  /// - `textDirection`: Determines the order to lay children out horizontally
+  /// and how to interpret `start` and `end` in the horizontal direction.
+  /// Defaults to the ambient [Directionality].
+  /// - `textBaseline`: If aligning items according to their baseline, which
+  /// baseline to use.
   SuperRow({
     Key key,
     AlignmentGeometry alignment = AlignmentDirectional.topStart,
@@ -36,6 +69,10 @@ class SuperRow extends BaseContainer {
     BoxConstraints constraints,
     int flex,
     bool ignoreImplicitWidthHeight = false,
+    bool enableInkWell = false,
+    Color splashColor,
+    VoidCallback onPressed,
+    VoidCallback onLongPressed,
     Matrix4 transform,
     Key childKey,
     List<Widget> children,
@@ -57,6 +94,8 @@ class SuperRow extends BaseContainer {
           constraints: constraints,
           flex: flex,
           ignoreImplicitWidthHeight: ignoreImplicitWidthHeight,
+          onPressed: onPressed,
+          onLongPressed: onLongPressed,
           margin: margin,
           transform: transform,
           child: Row(
