d0c3cc8962363c0fecc97610c2f535227637a781
Triet Bui <x3cafe@gmail.com>
2019-11-05 11:52:34 +0700

    SuperStyle: TextStyleBold: don't force require `color` and `fontSize`

--- lib/super_style.dart
+++ lib/super_style.dart
@@ -3,10 +3,8 @@ import 'package:flutter/widgets.dart';
 /// TextStyleBold is a [TextStyle] with [FontWeight] is bold
 class TextStyleBold extends TextStyle {
   /// Create new text style with font weight Bold
-  const TextStyleBold({@required Color color, @required double fontSize})
-      : assert(color != null),
-        assert(fontSize != null),
-        super(
+  const TextStyleBold({Color color, double fontSize})
+      : super(
           color: color,
           fontWeight: FontWeight.bold,
           fontSize: fontSize,
