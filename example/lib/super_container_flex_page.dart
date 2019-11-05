f77453c1e8c342665542353a86b2fcd4f835f6c2
Triet Bui <x3cafe@gmail.com>
2019-11-05 11:27:35 +0700

    SuperContainer: flex: implement it later

--- example/lib/super_container_flex_page.dart
+++ example/lib/super_container_flex_page.dart
@@ -17,76 +17,32 @@ class SuperContainerFlexPage extends StatelessWidget {
   /// Content of this page
   Widget buildBody(BuildContext context) {
     final boxH = SizedBox(height: 10);
-    final Color bgColor = Colors.yellowAccent;
+    const Color bgColor = Colors.yellowAccent;
 
     return SuperScrollViewColumn(
-      crossAxisAlignment: CrossAxisAlignment.start,
       padding: EdgeInsets.all(10),
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        Text('Below is:no dynamicSize, no with, no height'),
-        _wrapInContainer(
-          SuperContainer(
-            color: bgColor,
-            child: Text('SuperContent:no dynamicSize, no with, no height'),
-          ),
-        ),
-        boxH,
-        Text('Below is:dynamicSize=full, no width, no height'),
-        _wrapInContainer(
+        Text('Below is:flex=full, no width, no height'),
+        Text('Will implement later'),
+        /*_wrapInContainer(
           SuperContainer(
             color: bgColor,
             flex: 1,
             child: Text('SuperContent:dynamicSize=full, no width, no height'),
           ),
-        ),
-        boxH,
-        Text('Below is:dynamicSize=full, width=infinity, no height'),
-        _wrapInContainer(
-          SuperContainer(
-            color: bgColor,
-            flex: 1,
-            width: double.infinity,
-            child: Text('dynamicSize=full, width=infinity, no height'),
-          ),
-        ),
-        boxH,
-        Text('Below is:dynamicSize=full, width=150, no height'),
-        _wrapInContainer(
-          SuperContainer(
-            color: bgColor,
-            flex: 1,
-            width: 150,
-            child: Text('dynamicSize=full, width=150, no height'),
-          ),
-        ),
-        boxH,
-        Text('Below is:dynamicSize=full, width=infinity, height=30'),
-        _wrapInContainer(
-          SuperContainer(
-            color: bgColor,
-            flex: 1,
-            width: double.infinity,
-            height: 30,
-            child: Text('dynamicSize=full, width=infinity, height=30'),
-          ),
-        ),
-        boxH,
-        Text('So when SuperContainer has dynamicSize=full, then height '
-            'value is ignored and already be infinity'),
+        ),*/
         boxH,
       ],
     );
   }
 
   Widget _wrapInContainer(Widget child) {
-    return child;
-    /*return Container(
+    return Container(
       color: Colors.greenAccent,
       width: double.infinity,
       height: 70,
       child: child,
-    );*/
+    );
   }
 }
