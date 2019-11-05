6e84bd6e85978e73f57d44476849dccdfd0c6dfd
Triet Bui <x3cafe@gmail.com>
2019-11-05 10:03:00 +0700

    Fix Lint warning: `always_require_non_null_named_parameters`

--- lib/super_style.dart
+++ lib/super_style.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 /// TextStyleBold is a [TextStyle] with [FontWeight] is bold
 class TextStyleBold extends TextStyle {
   /// Create new text style with font weight Bold
-  const TextStyleBold({Color color, double fontSize})
+  const TextStyleBold({@required Color color, @required double fontSize})
       : assert(color != null),
         assert(fontSize != null),
         super(
