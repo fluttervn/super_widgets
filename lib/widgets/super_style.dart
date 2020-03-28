4deae6043199748eb30a2855d56d86fcec149cd2
Triet Bui Minh <x3cafe@gmail.com>
2020-03-28 17:25:11 +0700

    Add param `fontFamily` to `TextStyleBold`

--- lib/widgets/super_style.dart
+++ lib/widgets/super_style.dart
@@ -3,10 +3,11 @@ import 'package:flutter/widgets.dart';
 /// TextStyleBold is a [TextStyle] with [FontWeight] is bold
 class TextStyleBold extends TextStyle {
   /// Create new text style with font weight Bold
-  const TextStyleBold({Color color, double fontSize})
+  const TextStyleBold({Color color, double fontSize, String fontFamily})
       : super(
           color: color,
           fontWeight: FontWeight.bold,
           fontSize: fontSize,
+          fontFamily: fontFamily,
         );
 }
