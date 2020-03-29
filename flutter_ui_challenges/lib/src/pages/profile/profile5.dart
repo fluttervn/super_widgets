5eff8ae768766910b6d86be4eec9418a21116419
Triet Bui Minh <x3cafe@gmail.com>
2020-03-29 15:27:30 +0700

    ProfileFivePage: finish refactor with SuperWidget

--- flutter_ui_challenges/lib/src/pages/profile/profile5.dart
+++ flutter_ui_challenges/lib/src/pages/profile/profile5.dart
@@ -5,7 +5,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
+import 'package:flutter_ui_challenges/src/utils/toast_util.dart';
 import 'package:flutter_ui_challenges/src/widgets/network_image.dart';
+import 'package:flutter_ui_challenges/super_widgets/super_widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileFivePage extends StatelessWidget {
@@ -16,172 +18,162 @@ class ProfileFivePage extends StatelessWidget {
     final Color color1 = Color(0xffFC5CF0);
     final Color color2 = Color(0xffFE8852);
     final String image = avatars[0];
+
+    var gradientBackgroundWidget = Container(
+      height: 360,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+
+    var bottomToolbar = SuperStack(
+      children: <Widget>[
+        SuperRow(
+          pPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+          pMargin: EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
+          pDecoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color1, color2],
+            ),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          children: <Widget>[
+            IconButton(
+              color: Colors.white,
+              icon: Icon(FontAwesomeIcons.user),
+              onPressed: showDefaultToast,
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.location_on),
+              onPressed: showDefaultToast,
+            ),
+            Spacer(),
+            IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.add),
+              onPressed: showDefaultToast,
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.message),
+              onPressed: showDefaultToast,
+            ),
+          ],
+        ),
+        Center(
+          child: FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: showDefaultToast,
+            child: Icon(Icons.favorite, color: Colors.pink),
+          ),
+        ),
+      ],
+    );
+    var profileInfoWidget = SuperColumn(
+      pMargin: const EdgeInsets.only(top: 80),
+      children: <Widget>[
+        Text(
+          'Date mate',
+          style: TextStyleItalic(color: Colors.white, fontSize: 28),
+        ),
+        SizedBox(height: 20),
+        SuperStack(
+          pFlex: 1,
+          children: <Widget>[
+            SuperClipRRect(
+              pHeight: double.infinity,
+              pMargin: const EdgeInsets.only(left: 30, right: 30, top: 10),
+              borderRadius: BorderRadius.circular(30),
+              child: PNetworkImage(
+                image,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SuperText(
+              '3.7mi away',
+              pAlignment: Alignment.topCenter,
+              cPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              cDecoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            )
+          ],
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Sasha - 22',
+          style: TextStyleBold(fontSize: 16),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.location_on,
+              size: 16,
+              color: Colors.grey,
+            ),
+            Text(
+              'San Diego, California, USA',
+              style: TextStyle(color: Colors.grey.shade600),
+            )
+          ],
+        ),
+        SizedBox(height: 5),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            IconButton(
+              color: Colors.grey,
+              icon: Icon(FontAwesomeIcons.instagram),
+              onPressed: showDefaultToast,
+            ),
+            IconButton(
+              color: Colors.grey,
+              icon: Icon(FontAwesomeIcons.facebookF),
+              onPressed: showDefaultToast,
+            ),
+            IconButton(
+              color: Colors.grey.shade600,
+              icon: Icon(FontAwesomeIcons.twitter),
+              onPressed: showDefaultToast,
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        bottomToolbar
+      ],
+    );
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Container(
-            height: 360,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50)),
-                gradient: LinearGradient(
-                    colors: [color1, color2],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight)),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 80),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Date mate',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontStyle: FontStyle.italic),
-                ),
-                SizedBox(height: 20),
-                Expanded(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        height: double.infinity,
-                        margin:
-                            const EdgeInsets.only(left: 30, right: 30, top: 10),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30),
-                            child: PNetworkImage(
-                              image,
-                              fit: BoxFit.cover,
-                            )),
-                      ),
-                      Container(
-                        alignment: Alignment.topCenter,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Text('3.7mi away'),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'Sasha - 22',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.location_on,
-                      size: 16,
-                      color: Colors.grey,
-                    ),
-                    Text(
-                      'San Diego, California, USA',
-                      style: TextStyle(color: Colors.grey.shade600),
-                    )
-                  ],
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(
-                      color: Colors.grey,
-                      icon: Icon(FontAwesomeIcons.instagram),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      color: Colors.grey,
-                      icon: Icon(FontAwesomeIcons.facebookF),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      color: Colors.grey.shade600,
-                      icon: Icon(FontAwesomeIcons.twitter),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                Container(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 16),
-                        margin: const EdgeInsets.only(
-                            top: 30, left: 20, right: 20, bottom: 20),
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [color1, color2],
-                            ),
-                            borderRadius: BorderRadius.circular(30)),
-                        child: Row(
-                          children: <Widget>[
-                            IconButton(
-                              color: Colors.white,
-                              icon: Icon(FontAwesomeIcons.user),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              color: Colors.white,
-                              icon: Icon(Icons.location_on),
-                              onPressed: () {},
-                            ),
-                            Spacer(),
-                            IconButton(
-                              color: Colors.white,
-                              icon: Icon(Icons.add),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              color: Colors.white,
-                              icon: Icon(Icons.message),
-                              onPressed: () {},
-                            ),
-                          ],
-                        ),
-                      ),
-                      Center(
-                        child: FloatingActionButton(
-                          backgroundColor: Colors.white,
-                          onPressed: () {},
-                          child: Icon(
-                            Icons.favorite,
-                            color: Colors.pink,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
+          gradientBackgroundWidget,
           AppBar(
             backgroundColor: Colors.transparent,
             elevation: 0,
             actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.notifications),
-                onPressed: () {},
+                onPressed: showDefaultToast,
               ),
               IconButton(
                 icon: Icon(Icons.menu),
-                onPressed: () {},
+                onPressed: showDefaultToast,
               ),
             ],
           ),
+          profileInfoWidget,
         ],
       ),
     );
