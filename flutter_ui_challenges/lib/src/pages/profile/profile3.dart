e801fa8dff17cc734487af2634c5e0c2d2cd56a0
Triet Bui Minh <x3cafe@gmail.com>
2020-03-29 09:55:34 +0700

    ProfileThreePage: fix some Lint warnings

--- flutter_ui_challenges/lib/src/pages/profile/profile3.dart
+++ flutter_ui_challenges/lib/src/pages/profile/profile3.dart
@@ -9,8 +9,8 @@ import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
 import 'package:flutter_ui_challenges/src/widgets/network_image.dart';
 
 class ProfileThreePage extends StatelessWidget {
-  static final String path = "lib/src/pages/profile/profile3.dart";
-  final image = avatars[1];
+  static const String path = 'lib/src/pages/profile/profile3.dart';
+  final _image = avatars[1];
 
   @override
   Widget build(BuildContext context) {
@@ -23,66 +23,66 @@ class ProfileThreePage extends StatelessWidget {
               height: 250,
               width: double.infinity,
               child: PNetworkImage(
-                image,
+                _image,
                 fit: BoxFit.cover,
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(16.0, 200.0, 16.0, 16.0),
+              margin: EdgeInsets.fromLTRB(16, 200, 16, 16),
               child: Column(
                 children: <Widget>[
                   Stack(
                     children: <Widget>[
                       Container(
-                        padding: EdgeInsets.all(16.0),
-                        margin: EdgeInsets.only(top: 16.0),
+                        padding: EdgeInsets.all(16),
+                        margin: EdgeInsets.only(top: 16),
                         decoration: BoxDecoration(
                             color: Colors.white,
-                            borderRadius: BorderRadius.circular(5.0)),
+                            borderRadius: BorderRadius.circular(5)),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Container(
-                              margin: EdgeInsets.only(left: 96.0),
+                              margin: EdgeInsets.only(left: 96),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
                                   Text(
-                                    "Little Butterfly",
+                                    'Little Butterfly',
                                     style: Theme.of(context).textTheme.title,
                                   ),
                                   ListTile(
                                     contentPadding: EdgeInsets.all(0),
-                                    title: Text("Product Designer"),
-                                    subtitle: Text("Kathmandu"),
+                                    title: Text('Product Designer'),
+                                    subtitle: Text('Kathmandu'),
                                   ),
                                 ],
                               ),
                             ),
-                            SizedBox(height: 10.0),
+                            SizedBox(height: 10),
                             Row(
                               children: <Widget>[
                                 Expanded(
                                   child: Column(
-                                    children: <Widget>[
-                                      Text("285"),
-                                      Text("Likes")
+                                    children: const <Widget>[
+                                      Text('285'),
+                                      Text('Likes')
                                     ],
                                   ),
                                 ),
                                 Expanded(
                                   child: Column(
-                                    children: <Widget>[
-                                      Text("3025"),
-                                      Text("Comments")
+                                    children: const <Widget>[
+                                      Text('3025'),
+                                      Text('Comments')
                                     ],
                                   ),
                                 ),
                                 Expanded(
                                   child: Column(
-                                    children: <Widget>[
-                                      Text("650"),
-                                      Text("Favourites")
+                                    children: const <Widget>[
+                                      Text('650'),
+                                      Text('Favourites')
                                     ],
                                   ),
                                 ),
@@ -95,50 +95,50 @@ class ProfileThreePage extends StatelessWidget {
                         height: 80,
                         width: 80,
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10.0),
+                            borderRadius: BorderRadius.circular(10),
                             image: DecorationImage(
-                                image: CachedNetworkImageProvider(image),
+                                image: CachedNetworkImageProvider(_image),
                                 fit: BoxFit.cover)),
-                        margin: EdgeInsets.only(left: 16.0),
+                        margin: EdgeInsets.only(left: 16),
                       ),
                     ],
                   ),
-                  SizedBox(height: 20.0),
+                  SizedBox(height: 20),
                   Container(
                     decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(5.0),
+                      borderRadius: BorderRadius.circular(5),
                     ),
                     child: Column(
                       children: <Widget>[
                         ListTile(
-                          title: Text("User information"),
+                          title: Text('User information'),
                         ),
                         Divider(),
                         ListTile(
-                          title: Text("Email"),
-                          subtitle: Text("butterfly.little@gmail.com"),
+                          title: Text('Email'),
+                          subtitle: Text('butterfly.little@gmail.com'),
                           leading: Icon(Icons.email),
                         ),
                         ListTile(
-                          title: Text("Phone"),
-                          subtitle: Text("+977-9815225566"),
+                          title: Text('Phone'),
+                          subtitle: Text('+977-9815225566'),
                           leading: Icon(Icons.phone),
                         ),
                         ListTile(
-                          title: Text("Website"),
-                          subtitle: Text("https://www.littlebutterfly.com"),
+                          title: Text('Website'),
+                          subtitle: Text('https://www.littlebutterfly.com'),
                           leading: Icon(Icons.web),
                         ),
                         ListTile(
-                          title: Text("About"),
+                          title: Text('About'),
                           subtitle: Text(
-                              "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, illo repellendus quas beatae reprehenderit nemo, debitis explicabo officiis sit aut obcaecati iusto porro? Exercitationem illum consequuntur magnam eveniet delectus ab."),
+                              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, illo repellendus quas beatae reprehenderit nemo, debitis explicabo officiis sit aut obcaecati iusto porro? Exercitationem illum consequuntur magnam eveniet delectus ab.'),
                           leading: Icon(Icons.person),
                         ),
                         ListTile(
-                          title: Text("Joined Date"),
-                          subtitle: Text("15 February 2019"),
+                          title: Text('Joined Date'),
+                          subtitle: Text('15 February 2019'),
                           leading: Icon(Icons.calendar_view_day),
                         ),
                       ],
