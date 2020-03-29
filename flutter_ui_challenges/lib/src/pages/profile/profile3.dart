1b89ef5c7e277cb905c2f28918762d44e7df4aca
Triet Bui Minh <x3cafe@gmail.com>
2020-03-29 11:00:05 +0700

    ProfileThreePage: using SuperColumn

--- flutter_ui_challenges/lib/src/pages/profile/profile3.dart
+++ flutter_ui_challenges/lib/src/pages/profile/profile3.dart
@@ -28,125 +28,128 @@ class ProfileThreePage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Container(
-            margin: EdgeInsets.fromLTRB(16, 200, 16, 16),
-            child: Column(
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      margin: EdgeInsets.only(top: 16),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5)),
-                      child: Column(
+          SuperColumn(
+            pMargin: EdgeInsets.fromLTRB(16, 200, 16, 16),
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  SuperColumn(
+                    pPadding: EdgeInsets.all(16),
+                    pMargin: EdgeInsets.only(top: 16),
+                    pDecoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SuperColumn(
+                        pMargin: EdgeInsets.only(left: 96),
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(left: 96),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  'Little Butterfly',
-                                  style: context.textThemeTitle,
-                                ),
-                                ListTile(
-                                  contentPadding: EdgeInsets.all(0),
-                                  title: Text('Product Designer'),
-                                  subtitle: Text('Kathmandu'),
-                                ),
-                              ],
-                            ),
+                          Text(
+                            'Little Butterfly',
+                            style: context.textThemeTitle,
                           ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: <Widget>[
-                              Expanded(
-                                child: Column(
-                                  children: const <Widget>[
-                                    Text('285'),
-                                    Text('Likes')
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: const <Widget>[
-                                    Text('3025'),
-                                    Text('Comments')
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: const <Widget>[
-                                    Text('650'),
-                                    Text('Favourites')
-                                  ],
-                                ),
-                              ),
+                          ListTile(
+                            contentPadding: EdgeInsets.all(0),
+                            title: Text('Product Designer'),
+                            subtitle: Text('Kathmandu'),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: <Widget>[
+                          SuperColumn(
+                            pFlex: 1,
+                            children: const <Widget>[
+                              Text('285'),
+                              Text('Likes')
+                            ],
+                          ),
+                          SuperColumn(
+                            pFlex: 1,
+                            children: const <Widget>[
+                              Text('3025'),
+                              Text('Comments')
+                            ],
+                          ),
+                          SuperColumn(
+                            pFlex: 1,
+                            children: const <Widget>[
+                              Text('650'),
+                              Text('Favourites')
                             ],
                           ),
                         ],
                       ),
-                    ),
-                    Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: CachedNetworkImageProvider(_image),
-                              fit: BoxFit.cover)),
-                      margin: EdgeInsets.only(left: 16),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('User information'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('Email'),
-                        subtitle: Text('butterfly.little@gmail.com'),
-                        leading: Icon(Icons.email),
-                      ),
-                      ListTile(
-                        title: Text('Phone'),
-                        subtitle: Text('+977-9815225566'),
-                        leading: Icon(Icons.phone),
-                      ),
-                      ListTile(
-                        title: Text('Website'),
-                        subtitle: Text('https://www.littlebutterfly.com'),
-                        leading: Icon(Icons.web),
-                      ),
-                      ListTile(
-                        title: Text('About'),
-                        subtitle: Text(
-                            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, illo repellendus quas beatae reprehenderit nemo, debitis explicabo officiis sit aut obcaecati iusto porro? Exercitationem illum consequuntur magnam eveniet delectus ab.'),
-                        leading: Icon(Icons.person),
-                      ),
-                      ListTile(
-                        title: Text('Joined Date'),
-                        subtitle: Text('15 February 2019'),
-                        leading: Icon(Icons.calendar_view_day),
-                      ),
                     ],
                   ),
-                )
-              ],
-            ),
+                  Container(
+                    height: 80,
+                    width: 80,
+                    margin: EdgeInsets.only(left: 16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: CachedNetworkImageProvider(_image),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              SuperColumn(
+                pDecoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                children: <Widget>[
+                  ListTile(
+                    title: Text('User information'),
+                  ),
+                  Divider(),
+                  SuperListTile(
+                    title: Text('Email'),
+                    subtitle: Text('butterfly.little@gmail.com'),
+                    leading: Icon(Icons.email),
+                    onTap: showDefaultToast,
+                  ),
+                  ListTile(
+                    title: Text('Phone'),
+                    subtitle: Text('+977-9815225566'),
+                    leading: Icon(Icons.phone),
+                    onTap: showDefaultToast,
+                  ),
+                  ListTile(
+                    title: Text('Website'),
+                    subtitle: Text('https://www.littlebutterfly.com'),
+                    leading: Icon(Icons.web),
+                    onTap: showDefaultToast,
+                  ),
+                  ListTile(
+                    title: Text('About'),
+                    subtitle: Text('Lorem ipsum, dolor sit amet consectetur '
+                        'adipisicing elit. Nulla, illo repellendus quas beatae '
+                        'reprehenderit nemo, debitis explicabo officiis sit aut'
+                        ' obcaecati iusto porro? Exercitationem illum '
+                        'consequuntur magnam eveniet delectus ab. Lorem ipsum, '
+                        'dolor sit amet consectetur. Nulla, illo repellendus '
+                        'quas beatae reprehenderit nemo, debitis explicabo '
+                        'officiis sit aut obcaecati iusto porro elit'),
+                    leading: Icon(Icons.person),
+                    onTap: showDefaultToast,
+                  ),
+                  ListTile(
+                    title: Text('Joined Date'),
+                    subtitle: Text('15 February 2019'),
+                    leading: Icon(Icons.calendar_view_day),
+                    onTap: showDefaultToast,
+                  ),
+                ],
+              )
+            ],
           ),
           AppBar(
             backgroundColor: Colors.transparent,
